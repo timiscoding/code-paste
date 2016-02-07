922859580cbff330be9d2289e58c11aecbdf6da5
Timothy Tsui <timiscoding@gmail.com>
2016-02-08 10:26:48 +1100

    Show code element. Bug: converts characters to HTML escaped entities

--- app/assets/javascripts/elements.js
+++ app/assets/javascripts/elements.js
@@ -1,60 +1,28 @@
 $(document).ready(function() {
   console.log('running js');
-  aloha(document.querySelector('.element .content')); // make elements editable
 
-  // aloha auto assigns an id to the editables
-  var editables = aloha.dom.query('.content', document).map(aloha);
-
-  // bind buttons to corresponding editable
-  editables.forEach( function( editable ) {
-    for ( var command in aloha.ui.commands ) {
-      $( '.action-' + command + '-' + editable.id ).on(
-          'click',
-          aloha.ui.command( editable, aloha.ui.commands[command] ));
-    }
-  });
-
-  // when the cursor is on a formatted piece of text, change the look of the
-  // button to reflect that state
-  function middleware(event) {
-    $('.active').removeClass('active');
-    if ('leave' !== event.type) {
-      console.log('target', event);
-      var states = aloha.ui.states(aloha.ui.commands, event);
-      for (var selector in states) {
-        $('.action-' + selector + '-' + event.editable.id)
-          .toggleClass('active', states[selector]);
-      }
-    }
-    return event;
-  }
-
-  aloha.editor.stack.unshift(middleware);
-
-  var command = {
-    crazyCoolProperty: "You selected: ",
-    action: function (boundaries, selection, command, event) {
-      console.log(command.crazyCoolProperty + aloha.markers.hint(boundaries));
-      console.log('bounds', boundaries, selection, command, event, this);
-
-    }
-  };
-  $('#cool-btn').on('click', aloha.ui.command(command));
-
+  // create a new editor and configure it
   var editor = CodeMirror(document.getElementById('code'), {
     value: 'var x = 1',
     tabSize: 2,
     lineNumbers: true
   });
-  console.log( 'code contente ', editor.getValue() );
-  editor.setValue('var bunnies = 100;');
-  editor.setSize(500, 100);
 
+  // testing getter and setter and size
+  if ( typeof app !== 'undefined' && typeof app.content !== 'undefined') {
+    console.log( 'code contente ', app.content );
+    editor.setValue(app.content);
+  }
+  // editor.setSize(500, 100);
+
+  // when user types in editor, fire event
   editor.on('changes', function(instance, changes){
     console.log('editor changed', instance.getWrapperElement().parentNode);
   });
 
-  $('.font-size').on('click', function() {
+  // when a font size button is pressed, increase/decrease font size
+  $('.font-size').on('click', function(e) {
+    e.preventDefault();
     var cur_size = parseInt( $('.CodeMirror').css('font-size') );
     if ( $(this).text() === '+' ) {
     console.log('font size inc', cur_size);
@@ -63,9 +31,9 @@ $(document).ready(function() {
       console.log('dec size');
       $('.CodeMirror').css('font-size', (cur_size - 2) + 'px');
     }
-
   });
 
+  // when a theme or language is selected, update the editor
   console.log('modes', CodeMirror.modes, CodeMirror.mimeModes);
   $('#code').on('change', 'select', function(event) {
     console.log('select changed');
@@ -76,5 +44,34 @@ $(document).ready(function() {
     } else if ( setting === 'language') {
       editor.setOption('mode', settingVal );
     }
+  });
+
+  // send an AJAX POST request to create a new element
+  $('#code .save').on('click', function(e) {
+    e.preventDefault();
+    //title, :page_id, :content, :link, :pos_x, :pos_y, :width, :height)
+    var $codeElement = $( this ).closest('#code');
+    var url = '/elements';
+    var code = {
+      element: {
+        title: 'new element',
+        page_id: 100,
+        content: editor.getValue(),
+        link: 'www.google.com',
+        pos_x: 1,
+        pos_y: 2,
+        width: 500,
+        height: 100
+      }
+    };
+    // console.log(['save this', url, '|', JSON.stringify(code, null, 4), '|'].join(' '));
+    $.ajax('/elements', {
+      method: 'post',
+      dataType: 'json',
+      data: code
+    }).done(function( response ){
+      console.log('saved', response);
+      $codeElement.data('id', response.id);
+    });
   });
 });
