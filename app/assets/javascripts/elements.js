4e1ec86e0eac18a4722b15831dbab6939a126913
Timothy Tsui <timiscoding@gmail.com>
2016-02-08 20:04:11 +1100

    refactored

--- app/assets/javascripts/elements.js
+++ app/assets/javascripts/elements.js
@@ -1,81 +1,11 @@
+var app = app || {};
+
 _.templateSettings = {
  evaluate : /\{\[([\s\S]+?)\]\}/g,     // {[ console.log("Hello"); ]} - runs
  interpolate : /\{\{([\s\S]+?)\}\}/g   // {{ key }} - interpolates
 };
 
-$(document).ready(function() {
-  console.log('running js');
-  // add code editor template
-
-  var codeTemplater = _.template( $('#codeTemplate').html() );
-
-  // add template to page and include element id if it exists
-  var element_route = window.location.href.match( /elements\/(\d+)$/ );
-  // var element_route = [null, '27'];
-  console.log('elt route ', element_route);
-  if ( element_route !== null ){
-    $('body').append( codeTemplater({ element_id: element_route[1] }) );
-  } else {
-    $('body').append( codeTemplater({ element_id: ""}) );
-  }
-
-  // add code editor to template
-  // create a new editor and configure it
-  // console.log( 'editor', document.getElementsById('code') );
-  var editor = CodeMirror(function (editor) {
-      console.log('editor', editor);
-      $('.code').last().append( editor );
-    }, {
-      value: '// your code here',
-      tabSize: 2,
-      lineNumbers: true
-  });
-
-
-  // testing getter and setter and size
-  if ( element_route !== null ){
-    var elt_id = element_route[1];
-    console.log('matches ', element_route);
-    codeTemplater({ element_id: elt_id });
-    $.getJSON('/elements/' + element_route[1], function (data, responseStatus, jqXHR) {
-      editor.setValue(data.content);
-    });
-  }
-  // editor.setSize(500, 100);
-
-  // when user types in editor, fire event
-  editor.on('changes', function(instance, changes){
-    console.log('editor changed', instance.getWrapperElement().parentNode);
-  });
-
-  // when a font size button is pressed, increase/decrease font size
-  $('.font-size').on('click', function(e) {
-    e.preventDefault();
-    var cur_size = parseInt( $('.CodeMirror').css('font-size') );
-    if ( $(this).text() === '+' ) {
-    console.log('font size inc', cur_size);
-      $('.CodeMirror').css('font-size', (cur_size + 2) + 'px');
-    } else {
-      console.log('dec size');
-      $('.CodeMirror').css('font-size', (cur_size - 2) + 'px');
-    }
-  });
-
-  // when a theme or language is selected, update the editor
-  console.log('modes', CodeMirror.modes, CodeMirror.mimeModes);
-  $('.code').on('change', 'select', function(event) {
-    console.log('select changed');
-    var setting = $(this).attr('id');
-    var settingVal = $(this).val();
-    if ( setting === 'theme' ) {
-      editor.setOption('theme', settingVal );
-    } else if ( setting === 'language') {
-      editor.setOption('mode', settingVal );
-    }
-  });
-
-  // send an AJAX POST request to create a new element
-  $('.code .save').on('click', function(e) {
+var createUpdateElement = function (e) {
     e.preventDefault();
     var $saveButton = $( this );
     $saveButton.prop('disabled', true);
@@ -87,7 +17,7 @@ $(document).ready(function() {
       element: {
         title: 'new element',
         page_id: 100,
-        content: editor.getValue(),
+        content: app.editor.getValue(),
         link: 'www.google.com',
         pos_x: 1,
         pos_y: 2,
@@ -120,5 +50,75 @@ $(document).ready(function() {
     }).always(function() {
       $saveButton.prop('disabled', false);
     });
+};
+
+var changeLanguageOrTheme = function(event) {
+  console.log('select changed');
+  var setting = $(this).attr('id');
+  var settingVal = $(this).val();
+  if ( setting === 'theme' ) {
+    app.editor.setOption('theme', settingVal );
+  } else if ( setting === 'language') {
+    app.editor.setOption('mode', settingVal );
+  }
+};
+
+var changeFontSize = function(e) {
+  e.preventDefault();
+  var cur_size = parseInt( $('.CodeMirror').css('font-size') );
+  if ( $(this).text() === '+' ) {
+  console.log('font size inc', cur_size);
+    $('.CodeMirror').css('font-size', (cur_size + 2) + 'px');
+  } else {
+    console.log('dec size');
+    $('.CodeMirror').css('font-size', (cur_size - 2) + 'px');
+  }
+};
+
+$(document).ready(function() {
+  console.log('running js');
+  // get code element template
+  var codeTemplater = _.template( $('#codeTemplate').html() );
+  var $codeElement = $( codeTemplater() );
+  // simulate retrieve existing element from rails
+  var element_id = 42;
+  if ( element_id ){
+    console.log('retrieving element');
+    $.getJSON('/elements/' + element_id).done( function (data, responseStatus, jqXHR) {
+      app.editor.setValue(data.content);
+      $codeElement.data('id', element_id);
+    });
+  }
+  // editor.setSize(500, 100);
+  $('body').append( $codeElement );
+
+  // create a new editor and configure it
+  app.editor = CodeMirror(function (editor) {
+      console.log('editor', editor);
+      $codeElement.append( editor ); // add code editor to template
+    }, {
+      value: '// your code here',
+      tabSize: 2,
+      lineNumbers: true
   });
+
+
+
+
+  // when user types in editor, fire event
+  app.editor.on('changes', function(instance, changes){
+    console.log('editor changed', instance.getWrapperElement().parentNode);
+  });
+
+  // when a font size button is pressed, increase/decrease font size
+  $('.font-size').on('click', changeFontSize);
+
+  // when a theme or language is selected, update the editor
+  console.log('modes', CodeMirror.modes, CodeMirror.mimeModes);
+  $('.code').on('change', 'select', changeLanguageOrTheme);
+
+  // send an AJAX POST request to create/update a new element
+  $('.code .save').on('click', createUpdateElement);
+
+
 });
